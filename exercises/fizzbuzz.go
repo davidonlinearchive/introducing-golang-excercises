e51512979337a6bed5dedfbd301c3a4ef0de4ad9
davidonlinearchive <davidonlinearchive@gmail.com>
2025-05-20 08:41:40 -0400

    fixed typo

--- exercises/fizzbuzz.go
+++ exercises/fizzbuzz.go
@@ -10,7 +10,7 @@ import "fmt"
 func FizzBuzz() {
 	for i := 1; i < 100; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuz")
+			fmt.Println("FizzBuzz")
 		} else if i%3 == 0 {
 			fmt.Println("Fizz")
 		} else if i%5 == 0 {
