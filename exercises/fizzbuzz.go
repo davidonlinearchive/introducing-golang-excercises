6d2c9e218c42a793049b06f05c456fee7b92a288
davidonlinearchive <davidonlinearchive@gmail.com>
2025-05-20 03:50:20 -0400

    Fizz buzz update

--- exercises/fizzbuzz.go
+++ exercises/fizzbuzz.go
@@ -10,7 +10,7 @@ import "fmt"
 func FizzBuzz() {
 	for i := 1; i < 100; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz")
+			fmt.Println("FizzBuz")
 		} else if i%3 == 0 {
 			fmt.Println("Fizz")
 		} else if i%5 == 0 {
