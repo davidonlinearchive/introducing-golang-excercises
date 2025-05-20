11096b6ca6996b72df37475d645a092091666d9f
dob24 <dob24@njit.edu>
2025-05-20 03:45:10 -0400

    Fizz buzz

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "intro_go_lang/exercises"
 
 func main() {
-	exercises.ConvertToFahr()
-	exercises.ConvertToMeters()
+	//exercises.ConvertToFahr()
+	//exercises.ConvertToMeters()
+	exercises.FizzBuzz()
 }
