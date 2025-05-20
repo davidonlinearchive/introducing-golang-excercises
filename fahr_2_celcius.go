0dfd0930b0c386b2cc19f2cc729a89d5729470fe
dob24 <dob24@njit.edu>
2025-05-20 02:19:21 -0400

    Initial commit

---
+++ fahr_2_celcius.go
@@ -0,0 +1,17 @@
+//GOAL: write a program that converts from Fahrenheit into Celsius (C = (F − 32) * 5/9).
+
+package main
+
+import "fmt"
+
+func main() {
+	var fahr float32
+	var celcius float32
+
+	fmt.Print("Enter your degree in fahrenheit: ")
+	fmt.Scanln(&fahr)
+
+	celcius = (fahr - 32) * 5 / 9
+
+	fmt.Printf("Your degree is %.2f in celcius\n", celcius)
+}
